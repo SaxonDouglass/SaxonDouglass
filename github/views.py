ba22d37916f0aecf9134633f4b6d04cb157053c2
Saxon Douglass <saxon@saxondouglass.com>
2012-09-04 21:58:36 +0930

    Removed refs checking from github app

--- github/views.py
+++ github/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse, HttpResponseNotFound
 def post_receive(request):
     if request.method == 'POST':
         post = request.POST
-        if post.commits.ref == 'refs/heads/master':
-            subprocess.call(['github/post_receive.sh'])
+        #if post.commits.ref == 'refs/heads/master':
+        subprocess.call(['github/post_receive.sh'])
     return HttpResponse('')
